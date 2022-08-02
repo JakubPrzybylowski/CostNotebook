b5795a988840ce3d2659c1408d7000a62ea39f89
Jakub Przybyłowski <jakub.przybylowski95@gmail.com>
2022-08-02 15:00:31 +0200

    Create a basic backend and frontend and db

---
+++ codenotebook-frontend/costnotebook-backend/Models/CostNotebookDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace costnotebook_backend.Models
+{
+    public class CostNotebookDbContext : DbContext
+    {
+        public CostNotebookDbContext(DbContextOptions<CostNotebookDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Test> Tests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Test>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+        }
+    }
+}
